e30693aca091098cb2146a47e232907006e5895d
SM <SergioMedina>
2015-06-15 19:48:56 -0430

    validacion 1

--- app/models/requisition.rb
+++ app/models/requisition.rb
@@ -1,7 +1,9 @@
 class Requisition < ActiveRecord::Base
     mount_uploader :attachment, AttachmentUploader
 	validates_presence_of :solicitante, :message => "no puede estar en blanco." 
-	validates_presence_of :numero, :message => "del equipo no puede estar en blanco." 
+	validates_presence_of :numero, :message => "de requisicion no puede estar en blanco." 
 	validates_numericality_of :numero, :greater_than => 0, :message => "debe ser un numero mayor que 0."
+	validates_presence_of :created_at, :message => "no puede estar en blanco."
+	validates_presence_of :attachment, :message => "debe ser cargado en el campo Requisicion." 
 	belongs_to :user
 end
